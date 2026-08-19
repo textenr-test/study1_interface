b5080ce72ca721e0f917af72a00624e0b35357b7
textenr-test <textenr@gmail.com>
2026-08-19 20:58:52 +0900

    Refine study instructions and participant log privacy

--- study-config.js
+++ study-config.js
@@ -1,5 +1,5 @@
 window.STUDY_CONFIG = Object.freeze({
-  version: "2026-08-19-v2",
+  version: "2026-08-19-v3",
   title: "Visual First-Impression Study",
   targetParticipants: 30,
   trialCount: 38,
